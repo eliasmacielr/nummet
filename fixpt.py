c340d0993df80f874b72b2b129d4d1531c2593e3
Elias Maciel <eliasmacielr@gmail.com>
2019-04-11 13:06:25 -0400

    Fixed-Point Iteration
    Implement the fixed-point iteration method as shown in Numerical
    Methods for Engineers (Chapra, Canale) p. 151.

---
+++ fixpt.py
@@ -0,0 +1,17 @@
+import math
+
+def fixpt(x0, es, imax):
+    xr = x0
+    it = 0
+    while True:
+        xrold = xr
+        xr = g(xrold)
+        it = it + 1
+        if xr != 0:
+            ea = abs((xr - xrold) / xr) * 100
+        if ea < es or it >= imax:
+            break
+    return xr, it, ea
+
+def g(x):
+    return math.exp(-x) # e^(-x)

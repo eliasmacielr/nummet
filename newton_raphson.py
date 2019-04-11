4ae0f10d5dd41a4e93ee1d48915550aeaf788976
Elias Maciel <eliasmacielr@gmail.com>
2019-04-11 13:10:20 -0400

    Newton-Raphson Method
    Implement the Newton-Raphson method as shown in Numerical Methods
    for Engineers (Chapra, Canale) p. 151, 152.

---
+++ newton_raphson.py
@@ -0,0 +1,21 @@
+import math
+
+def newton_raphson(x0, es, imax):
+    xr = x0
+    it = 0
+    while True:
+        xrold = xr
+        xr = xrold - f(xrold) / f_(xrold)
+        it = it + 1
+        if xr != 0:
+            ea = abs((xr - xrold) / xr) * 100
+        if ea < es or it >= imax:
+            break
+    return xr, it, ea
+
+
+def f(x):
+    return math.exp(-x) - x # e^(-x) - x
+
+def f_(x):
+    return (f(x+1e-6) - f(x)) / 1e-6

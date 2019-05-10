c2ca87084b975194494894daf13d93c43e07571c
Elias Maciel <eliasmacielr@gmail.com>
2019-05-10 11:39:00 -0400

    Correct Power method

--- power/power_iteration.py
+++ power/power_iteration.py
@@ -1,19 +1,17 @@
-import numpy.linalg.norm as norm
-import numpy.matmul as matmul
-import numpy.dot as dot
+import numpy as np
+import numpy.linalg as la
 
 
 def power_iteration(A, x, e): 
-    x = x / norm(x)
-    a = matmul(A, x)
-    l = dot(a, x)
+    x = x / la.norm(x)
+    a = np.matmul(A, x)
+    l = np.dot(a, x)
     i = 0
-    while norm(a - l * x) > e * abs(l):
-        x = a / norm(a)
-        a = matmul(A, x)
-        l = dot(a, x)
+    while la.norm(a - l * x) > e * abs(l):
+        x = a / np.norm(a)
+        a = np.matmul(A, x)
+        l = np.dot(a, x)
         i = i + 1
 
     x.insert(0, l)
     x.append(i)
-

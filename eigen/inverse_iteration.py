0548d051cdb3e1761ca517a99ccf800f49d23a6b
Elias Maciel <eliasmacielr@gmail.com>
2019-05-10 21:14:56 -0400

    Inverse Method
    Implement the Shifted Inverse Power method iteration as shown in
    Numerical Methods for Eigenvalue Problems (Borm, Mehl) p. 75.

--- eigen/inverse_iteration.py
+++ eigen/inverse_iteration.py
@@ -1,2 +1,23 @@
+from numpy.linalg import norm
+from scipy.linalg import lu
+
 def inverse_iteration(A, x, e, u, f, maxit):
-    return None
+    if f == 0:
+        # Perform LU factorization
+    elif f == 1:
+        # Perform QR factorization
+    x = x / norm(x)
+    # Solve (A - uI)a = x
+    l_ = dot(a, x)
+    l = 1/l_ + u
+    i = 0
+    while norm(a - l_*x) > e * abs(l_):
+        x = x / norm(x)
+        # Solve (A - uI)a = x
+        l_ = dot(a, x)
+        l = 1/l_ + u
+        i = i + 1
+
+    x = insert(x, 0, l)
+    x = append(x, i)
+    return x

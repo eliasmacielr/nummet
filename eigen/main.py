a23a966c13535ccc9d40e9637facb461563cd6d4
Elias Maciel <eliasmacielr@gmail.com>
2019-05-11 23:05:48 -0400

    Correct main
    - Add imports.
    
    - Add test matrix A.
    
    - Print result.

--- eigen/main.py
+++ eigen/main.py
@@ -1,31 +1,38 @@
 from time import time
-
+from numpy import append
 from numpy import array
+
+from power_iteration import power_iteration
+from inverse_iteration import inverse_iteration
 
 
 def main():
-    A = array([[1,0],
-               [0,1]])
-    s = 0
+    A = array([[4,-5],
+               [2,-3]])
+    s = 1
     f = 0
     e = 10e-5
     u = 500
     x = power_method(A, s, f, e, u)
+    print(x)
 
 
 def power_method(A, s, f, e, u):
-    x = array([0,0])
-    x_ = None
-    maxit = 100
+    x = array([1,0])
+    maxit = 1000
     start = None
     end = None
     if s == 0:
         start = time()
-        x_ = power_iteration(A, x, e, maxit)
+        x = power_iteration(A, x, e, maxit)
         end = time()
     if s == 1:
         start = time()
-        x_ = inverse_iteration(A, x, e, u, f, maxit)
+        x = inverse_iteration(A, x, e, u, f, maxit)
         end = time()
     x = append(x, end - start)
     return x
+
+
+if __name__ == '__main__':
+    main()

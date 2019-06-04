0ca65b5bcf16d260432f6fc61b4b34fa88c3a6b0
Elias Maciel <eliasmacielr@gmail.com>
2019-06-04 15:49:36 -0400

    Working with sparse matrix

--- diff/main.py
+++ diff/main.py
@@ -1,10 +1,12 @@
 import math
 import numpy as np
+from math import e
 from math import exp
 from numpy.linalg import norm
-from scipy.sparse.linalg import spsolve
+from numpy.linalg import solve
+from scipy.sparse import diags
 from scipy.sparse import csr_matrix
-from scipy.sparse import dia_matrix
+from scipy.sparse.linalg import spsolve
 
 
 def main():
@@ -17,37 +19,37 @@ def main():
     d_a = [] # above main diagonal
     b = []   # right-hand side
     Y = []   # solution
-    for j in range(1, N+1):
+
+    # The diagonal above the main one goes from 1 to N-1,
+    # the main diagonal goes from 1 to N,
+    # the diagonal below the main one goes from 2 to N.
+    tj = h + 1
+    d_m.append(-(2*(tj**2)/(h**2) + 1))
+    d_a.append((tj**2)/(h**2) + tj/(2*h))
+    b.append((tj**2)*exp(-tj))
+    Y.append(y(tj))
+    for j in range(2, N):
         tj = j*h + 1
-        #print(tj, end=' ')
-        aux_exp1 = (tj**2)/(h**2)
-        aux_exp2 = tj/(2*h)
-        d_b.append(aux_exp1 - aux_exp2)
-        d_m.append(-(2*aux_exp1 + 1))
-        d_a.append(aux_exp1 + aux_exp2)
-        b.append((tj**2) * math.exp(-tj))
+        d_b.append((tj**2)/(h**2) - tj/(2*h))
+        d_m.append(-(2*(tj**2)/(h**2) + 1))
+        d_a.append((tj**2)/(h**2) + tj/(2*h))
+        b.append((tj**2)*exp(-tj))
         Y.append(y(tj))
-    #print('')
+    tj = N*h + 1
+    d_b.append((tj**2)/(h**2) - tj/(2*h))
+    d_m.append(-(2*(tj**2)/(h**2) + 1))
+    b.append((tj**2)*exp(-tj))
+    Y.append(y(tj))
 
     # Sparse matrix, need CSR or CSC to use spsolve.
-    data = np.array([d_m,d_b,d_a])
-    offset = np.array([0,-1,1])
-    S = dia_matrix((data, offset), shape=(N,N))
-    S = csr_matrix(S)
-    #F = S.toarray()
+    S = csr_matrix(diags([d_b,d_m,d_a],[-1,0,1]))
     X = spsolve(S, b)
-    #print(np.allclose(S.dot(X), b))
-    print(norm(S.dot(X) - b))
-    print(norm(S.dot(X) - b, np.inf))
-    #print(np.allclose(X, Y))
     print(norm(Y - X))
-    #np.savetxt('X', X)
-    #np.savetxt('Y', Y)
 
 
 # Exact solution
 def y(x):
-    y = ((2*(x**2 - 10000))/exp(1) - (101*(x**2 - 1))/exp(100) + 9999*exp(-x)*(x + 1))/9999*x
+    y = (2/9999*e**(-1) - 101/9999*e**(-100))*x + (101/9999*e**(-100) - 20000/9999*e**(-1))*x**(-1) + e**(-x)*(1 + x**(-1))
     return y
 
 

b989efd55e426c94540be6c36b61e6a029f1e5e5
Elias Maciel <eliasmacielr@gmail.com>
2019-06-04 20:16:09 -0400

    Until your computer cannot handle the computation

--- diff/main.py
+++ diff/main.py
@@ -1,4 +1,5 @@
 import math
+import time
 import numpy as np
 from math import e
 from math import exp
@@ -10,41 +11,56 @@ from scipy.sparse.linalg import spsolve
 
 
 def main():
-    N = 50
-    h = (100-1)/(N+1)
+    flag = 0 # 1 for full and 0 for sparse
+    tol = 1e-12
+    N = 8000000
+    while True:
+        h = (100-1)/(N+1)
 
-    # Arrays for diagonals, right-hand side and solution
-    d_b = [] # below main diagonal
-    d_m = [] # main diagonal
-    d_a = [] # above main diagonal
-    b = []   # right-hand side
-    Y = []   # solution
+        # Arrays for diagonals, right-hand side and solution
+        d_b = [] # below main diagonal
+        d_m = [] # main diagonal
+        d_a = [] # above main diagonal
+        b = []   # right-hand side
+        Y = []   # exact solution
+        X = []   # numerical solution
 
-    # The diagonal above the main one goes from 1 to N-1,
-    # the main diagonal goes from 1 to N,
-    # the diagonal below the main one goes from 2 to N.
-    tj = h + 1
-    d_m.append(-(2*(tj**2)/(h**2) + 1))
-    d_a.append((tj**2)/(h**2) + tj/(2*h))
-    b.append((tj**2)*exp(-tj))
-    Y.append(y(tj))
-    for j in range(2, N):
-        tj = j*h + 1
-        d_b.append((tj**2)/(h**2) - tj/(2*h))
+        # The diagonal above the main one goes from 1 to N-1,
+        # the main diagonal goes from 1 to N,
+        # the diagonal below the main one goes from 2 to N.
+        tj = h + 1
         d_m.append(-(2*(tj**2)/(h**2) + 1))
         d_a.append((tj**2)/(h**2) + tj/(2*h))
         b.append((tj**2)*exp(-tj))
         Y.append(y(tj))
-    tj = N*h + 1
-    d_b.append((tj**2)/(h**2) - tj/(2*h))
-    d_m.append(-(2*(tj**2)/(h**2) + 1))
-    b.append((tj**2)*exp(-tj))
-    Y.append(y(tj))
+        for j in range(2, N):
+            tj = j*h + 1
+            d_b.append((tj**2)/(h**2) - tj/(2*h))
+            d_m.append(-(2*(tj**2)/(h**2) + 1))
+            d_a.append((tj**2)/(h**2) + tj/(2*h))
+            b.append((tj**2)*exp(-tj))
+            Y.append(y(tj))
+        tj = N*h + 1
+        d_b.append((tj**2)/(h**2) - tj/(2*h))
+        d_m.append(-(2*(tj**2)/(h**2) + 1))
+        b.append((tj**2)*exp(-tj))
+        Y.append(y(tj))
 
-    # Sparse matrix, need CSR or CSC to use spsolve.
-    S = csr_matrix(diags([d_b,d_m,d_a],[-1,0,1]))
-    X = spsolve(S, b)
-    print(norm(Y - X))
+        # Sparse matrix, need CSR or CSC to use spsolve.
+        S = csr_matrix(diags([d_b,d_m,d_a],[-1,0,1]))
+        if flag == 0:
+            X = spsolve(S, b)
+        elif flag == 1:
+            # Full matrix
+            F = S.toarray()
+            X = solve(F, b)
+
+        if norm(Y - X, np.inf) < tol:
+            break
+        N += 1000
+        print(N)
+
+    print(flag, tol, N)
 
 
 # Exact solution
